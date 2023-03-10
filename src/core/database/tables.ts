d811d5e2e850767c806a2aa7144066a7e032e062
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-03-11 11:32:53 +1300

    Update: Added sql code to

--- src/core/database/tables.ts
+++ src/core/database/tables.ts
@@ -1,5 +1,24 @@
 import { pool } from "./pool";
 
-async function createTables(): Promise<any> {
+export async function createTables(): Promise<any> {
+  const query = `
+    CREATE TABLE IF NOT EXISTS users (
+	    uuid text PRIMARY KEY NOT NULL,
+	    username text NOT NULL,
+	    firstname text NOT NULL,
+	    lastname text NOT NULL,
+	    "password" text NOT NULL,
+	    email text NOT NULL,
+  	  phonenumber text NOT NULL,
+      type text NOT NULL
+    );
+  `;
+
+  try {
+    pool.query(query);
+  } catch (err: any) {
+    return false;
+  }
+
   return true;
 }
