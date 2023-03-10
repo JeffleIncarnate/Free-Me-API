19234fc4ad22ed4657325dd580752eb78025ef31
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-03-11 11:40:56 +1300

    Feature: Made a function on startup to check the sus stuff of this code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import express, { Request, Response, Application } from "express";
+import chalk from "chalk";
+
+import { createTables } from "./core/database/tables";
+import { testConnecton } from "./core/database/pool";
 
 const app: Application = express();
 let port = 3000;
+const log = console.log;
+chalk.level = 1;
 
 app.use(express.json());
+
+// Rouths -- User
+const getUserData = require("./routes/users/getUserData");
+const createUser = require("./routes/users/createUser");
+
+// Use Routes -- User
+app.use("/freeme", getUserData);
+app.use("/freeme", createUser);
 
 app.get("/", async (req: Request, res: Response) => {
   return res.send({ detail: "Welcome to the Free me API" });
@@ -17,6 +31,19 @@ app.all("*", async (req: Request, res: Response) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+async function startUp() {
+  // performs all nessesary checks before starting
+  await testConnecton();
+  log(chalk.blue("Connection secure"), chalk.green("✓"));
+
+  await createTables();
+  log(chalk.blue("Tables Created"), chalk.green("✓"));
+
+  app.listen(port, () => {
+    log(chalk.blue(`Listening on port ${port}`));
+  });
+}
+
+(async () => {
+  await startUp();
+})();
