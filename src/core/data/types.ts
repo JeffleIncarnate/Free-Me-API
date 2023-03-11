4697f27b3bc336ed74c4b3f7852a67bb0ccfdd52
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-03-11 22:06:48 +1300

    Update: gotta get ALL the types

--- src/core/data/types.ts
+++ src/core/data/types.ts
@@ -18,6 +18,26 @@ export type User = {
   };
 };
 
+export type VerifyEmail = {
+  uuid: string;
+  username: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phonenumber: string;
+  password: string;
+  type: string;
+  scopes: {
+    getSelf: false;
+    mofifySelf: false;
+    deleteSelf: false;
+    getOtherUsers: false;
+    createUsers: false;
+    deleteUsers: false;
+    updateUsers: false;
+  };
+};
+
 export type AccessTokenUser = {
   uuid: string;
   password: string;
