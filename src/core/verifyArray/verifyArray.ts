351fd4d0b5f394ced3b913a36bfdef3afdfe9e75
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2024-01-09 15:23:16 +1300

    chore: removed everything?

--- src/core/verifyArray/verifyArray.ts
+++
@@ -1,7 +0,0 @@
-export function verifyArray(items: string[]): boolean {
-  for (let i = 0; i < items.length; i++)
-    if (items[i] === null || items[i] === undefined || items[i] === "")
-      return false;
-
-  return true;
-}
