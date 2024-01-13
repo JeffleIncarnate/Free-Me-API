0e1c38d23e60571e0cdeefd1527cbebb5319945a
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2024-01-14 12:59:35 +1300

    feat: express request object has user type noew

--- src/core/types/environment.d.ts
+++ src/core/types/environment.d.ts
@@ -8,6 +8,15 @@ declare global {
       DATABASE_URL: string; // Prisma only but in case I every wanna do something with it
     }
   }
+
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        role: string;
+      };
+    }
+  }
 }
 
 export {};
