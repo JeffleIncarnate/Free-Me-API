a14d0898bed9a7b66ff5ddddfc6f24a8a704569f
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2024-01-13 17:06:20 +1300

    feat: routes

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,38 @@
+import "dotenv/config";
+
 import express, { NextFunction } from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 import { logger } from "./core/logger/logger";
+import { errorHandler } from "./core/errors/handler";
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+// Routes
+// Auth
+import { login } from "./core/auth/login";
+import { refresh } from "./core/auth/refresh";
+
+// User
+import { userPost } from "./routes/user/create/create";
+
+// Use Routes
+// Auth
+app.use("/v1/api/auth/login", login);
+app.use("/v1/api/auth/refresh", refresh);
+
+app.use("/v1/api/user", userPost);
 
 app.get("/", async (req, res) => {
   return res.sendStatus(200);
 });
 
-app.get("/elo", async (req, res, next) => {
-  return next(new HTTPErrors.Omg());
-});
+app.use(errorHandler);
 
 app.listen(3000, () => {
   logger.info("API running on port 3000");
