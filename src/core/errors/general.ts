eeff7527f2111f47cff97324db8faf2a3af5b25b
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2024-01-13 16:24:10 +1300

    feat: updated the prisma error to make it more descriptive and fixed the status code

--- src/core/errors/general.ts
+++ src/core/errors/general.ts
@@ -2,8 +2,9 @@ import { HTTPError } from "./base";
 
 export class PrismaErr extends HTTPError {
   constructor(prismaErr: string) {
-    super(400, {
-      reason: prismaErr,
+    super(500, {
+      reason: "Some error occurred while updating database",
+      err: prismaErr,
     });
   }
 }
