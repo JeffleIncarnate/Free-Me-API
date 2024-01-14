57499b239c73c5df2ec1725e7dbac1240ab4b6a0
Dhruv-Rayat1 <dhruvrayat50@gmail.com>
2024-01-14 13:01:44 +1300

    feat: oops

--- src/routes/user/get/self.ts
+++ src/routes/user/get/self.ts
@@ -14,72 +14,66 @@ self.get("/", authorizeRequest, async (req, res, next) => {
     );
   }
 
-  return next(
-    new Error(
-      "THE SAME FUCKING GUY CAME IN HERE WITH AN ALTERED TOKEN. FUCK OFF"
-    )
-  );
+  const user = await pool.user.findUnique({
+    where: {
+      id: req.user.id,
+    },
+    select: {
+      type: true,
+    },
+  });
 
-  // const user = await pool.user.findUnique({
-  //   where: {
-  //     id: req.user.id,
-  //   },
-  //   select: {
-  //     type: true,
-  //   },
-  // });
+  if (user === null) {
+    return next(
+      new Error("AGAIN WITH THE FAKE FUCKING TOKEN. I WILL IP BAN YO ASS")
+    );
+  }
 
-  // if (user === null) {
-  //   return next(
-  //     new Error("AGAIN WITH THE FAKE FUCKING TOKEN. I WILL IP BAN YO ASS")
-  //   );
-  // }
+  if (user.type === "CLIENT") {
+    const profile = await pool.user.findUnique({
+      where: {
+        id: req.user.id,
+      },
+      include: {
+        clientProfile: true,
+      },
+    });
 
-  // if (user.type === "CLIENT") {
-  //   const profile = await pool.user.findUnique({
-  //     where: {
-  //       id: req.user.id,
-  //     },
-  //     include: {
-  //       clientProfile: true,
-  //     },
-  //   });
+    if (profile === null) {
+      return next(new Error("Bro, go away"));
+    }
 
-  //   if (profile === null) {
-  //     return next(new Error("Bro, go away"));
-  //   }
+    return res.send({
+      success: true,
+      profile: {
+        ...profile,
+        nzbn: profile.nzbn.toString(),
+        gst: profile.gst.toString(),
+      },
+      status: 200,
+    });
+  }
 
-  //   return res.send({
-  //     success: true,
-  //     profile: {
-  //       ...profile,
-  //       nzbn: profile.nzbn.toString(),
-  //       gst: profile.gst.toString(),
-  //     },
-  //     status: 200,
-  //   });
-  // }
+  const profile = await pool.user.findUnique({
+    where: {
+      id: req.user.id,
+    },
+    include: {
+      consultantProfile: true,
+    },
+  });
 
-  // const profile = await pool.user.findUnique({
-  //   where: {
-  //     id: req.user.id,
-  //   },
-  //   include: {
-  //     consultantProfile: true,
-  //   },
-  // });
+  if (profile === null) {
+    return next(new Error("Bro, go away"));
+  }
 
-  // if (profile === null) {
-  //   return next(new Error("Bro, go away"));
-  // }
-
-  // return res.send({
-  //   success: true,
-  //   profile: {
-  //     ...profile,
-  //     nzbn: profile.nzbn.toString(),
-  //     gst: profile.gst.toString(),
-  //   },
-  //   status: 200,
-  // });
+  return res.send({
+    success: true,
+    profile: {
+      ...profile,
+      nzbn: profile.nzbn.toString(),
+      gst: profile.gst.toString(),
+    },
+    status: 200,
+  });
 });
